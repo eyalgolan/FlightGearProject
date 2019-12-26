ae9f6a8ac708f904b41f8af2f73a5503c4886b1d
eyalgolan <eyalgolan96@gmail.com>
2019-12-26 15:18:02 +0200

    starting to work on sending commands to simulator

--- Parser.cpp
+++ Parser.cpp
@@ -21,49 +21,47 @@ void Parser::runCommands() {
   int index = 0;
   vector<string> inputParams;
   while(index < this->inputVector.size()) {
-    Command* c = this->commandMap.find(this->inputVector[index])->second;
-    string commandName = this->inputVector[index];
-    if(c != nullptr) {
-      if(commandName.compare("openDataServer") == 0) {
-        inputParams.push_back(inputVector[index+1]);
-        cout<<commandName<<endl;
-        index += c->exec(inputParams);
-        inputParams.clear();
-
-      }
-      else if(commandName.compare("connectControlClient") == 0) {
-        inputParams.push_back(inputVector[index+1]);
-        inputParams.push_back(inputVector[index+2]);
-        cout<<commandName<<endl;
-        index += c->exec(inputParams);
-        inputParams.clear();
-      }
-      else if(commandName.compare("print") == 0) {
-        inputParams.push_back(inputVector[index+1]);
-        cout<<commandName<<endl;
-        index += c->exec(inputParams);
-        inputParams.clear();
-      }
-      else if(commandName.compare("var") == 0 && inputVector[index+2].compare("=") != 0) {
-        inputParams.push_back(inputVector[index+1]);
-        inputParams.push_back(inputVector[index+2]);
-        inputParams.push_back(inputVector[index+3]);
-        inputParams.push_back(inputVector[index+4]);
-        cout<<commandName<<endl;
-        index += c->exec(inputParams);
-        inputParams.clear();
-      //  cout<<"I'm in var command"<<endl;
-      }
-      else {
-        SymbolTable &symblTbl = SymbolTable::getInstance();
-        symblTbl.g_updateLock.lock();
-        if(symblTbl.getNameMap().find(commandName) != symblTbl.getNameMap().end()) {
-          inputParams.push_back(inputVector[index]);
-          inputParams.push_back(inputVector[index+2]);
-          cout<<commandName<<endl;
+    Command* c;
+    if(this->commandMap.find(this->inputVector[index]) != this->commandMap.end()) {
+      c = this->commandMap.find(this->inputVector[index])->second;
+      string commandName = this->inputVector[index];
+      if(c != nullptr) {
+        if (commandName.compare("openDataServer") == 0) {
+          inputParams.push_back(inputVector[index + 1]);
+          cout << commandName << endl;
           index += c->exec(inputParams);
           inputParams.clear();
+
+        } else if (commandName.compare("connectControlClient") == 0) {
+          inputParams.push_back(inputVector[index + 1]);
+          inputParams.push_back(inputVector[index + 2]);
+          cout << commandName << endl;
+          index += c->exec(inputParams);
+          inputParams.clear();
+        } else if (commandName.compare("print") == 0) {
+          inputParams.push_back(inputVector[index + 1]);
+          cout << commandName << endl;
+          index += c->exec(inputParams);
+          inputParams.clear();
+        } else if (commandName.compare("var") == 0
+            && inputVector[index + 2].compare("=") != 0) {
+          inputParams.push_back(inputVector[index + 1]);
+          inputParams.push_back(inputVector[index + 2]);
+          inputParams.push_back(inputVector[index + 3]);
+          inputParams.push_back(inputVector[index + 4]);
+          cout << commandName << endl;
+          index += c->exec(inputParams);
+          inputParams.clear();
+          //  cout<<"I'm in var command"<<endl;
         }
+      }
+      else {
+        c = this->commandMap.find("setVarCommand")->second;
+        inputParams.push_back(inputVector[index]);
+        inputParams.push_back(inputVector[index+2]);
+        cout<<commandName<<endl;
+        index += c->exec(inputParams);
+        inputParams.clear();
       }
 //     cout<<"parser next command"<<endl;
     }
